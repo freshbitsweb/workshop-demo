004ea9ffb7fca2246365efbe01ab9b3c9a1828fb
Rajan <rajan@freshbits.in>
2020-03-09 15:09:53 +0530

    players table migrate

---
+++ database/migrations/2020_03_09_092647_create_players_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50);
+            $table->integer('batting_average');
+            $table->integer('bowling_average');
+            $table->boolean('playing')->comment('0: Yes, 1: No');
+            $table->string('avatar');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}

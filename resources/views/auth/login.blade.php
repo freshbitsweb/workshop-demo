cb6228c959943c689c63ebbabee356376b0e7bf0
Rajan <rajan@freshbits.in>
2020-03-12 15:23:43 +0530

    html: remove unuse

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,9 +7,12 @@
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="row">
                         <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+
                         <div class="col-lg-6">
                             <div class="p-5">
-                            @include('includes.errors')
+
+                                @include('includes.errors')
+
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Login</h1>
                                 </div>
@@ -36,13 +39,13 @@
                                     </div>
 
                                     <button type="submit" class="btn btn-primary btn-user btn-block">
-                                        {{ __('Login') }}
+                                        Login
                                     </button>
 
                                     <div class="text-center">
                                         @if (Route::has('password.request'))
                                             <a class="btn btn-link" href="{{ route('password.request') }}">
-                                                {{ __('Forgot Your Password?') }}
+                                                Forgot Your Password?
                                             </a>
                                         @endif
                                     </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,12 +4,14 @@
     <div class="container">
         <div class="card o-hidden border-0 shadow-lg my-5">
             <div class="card-body p-0">
-                <!-- Nested Row within Card Body -->
                 <div class="row">
                     <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+
                     <div class="col-lg-7">
                         <div class="p-5">
+
                             @include('includes.errors')
+
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Register</h1>
                             </div>
@@ -56,7 +58,7 @@
                                 </div>
 
                                 <button type="submit" class="btn btn-primary btn-user btn-block">
-                                    {{ __('Register') }}
+                                   Register
                                 </button>
                             </form>
 

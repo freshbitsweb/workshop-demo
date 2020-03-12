22ac4a649f3840795158b9a6cea54f059a829b96
Gaurav Makhecha <gauravmakhecha@gmail.com>
2020-03-12 13:08:33 +0530

    refactor: cleanup

--- app/Player.php
+++ app/Player.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Player extends Model
 {
-    public $timestamps = false;
     /**
      * The attributes that are mass assignable.
      *
@@ -14,14 +13,5 @@ class Player extends Model
      */
     protected $fillable = [
         'name', 'batting_average', 'bowling_average', 'playing', 'avatar',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
     ];
 }
